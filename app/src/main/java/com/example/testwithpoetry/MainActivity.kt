2dd0c38dc386f82f71c87f93cd4399ef7e518f90
img <irma.de.y@gmail.com>
2025-03-04 13:40:21 -0600

    welcomeScreen created

--- app/src/main/java/com/example/testwithpoetry/MainActivity.kt
+++ app/src/main/java/com/example/testwithpoetry/MainActivity.kt
@@ -4,17 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.testwithpoetry.ui.screen.WelcomeScreen
 import com.example.testwithpoetry.ui.theme.TestWithPoetryTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,40 +19,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestWithPoetryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    WelcomeScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val viewModel: MainViewModel = hiltViewModel()
-    Box (
-        contentAlignment = Alignment.Center,
-        modifier = modifier.fillMaxSize()
-    ) {
-        Button(
-            onClick = {
-                viewModel.action()
-            },
-            modifier = modifier
-        ) {
-            Text(
-                text = "Try me $name",
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestWithPoetryTheme {
-        Greeting("Android")
-    }
-}
